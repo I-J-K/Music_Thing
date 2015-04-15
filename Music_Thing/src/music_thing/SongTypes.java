a46bbd418ee6a5a1ea0b110527e0f002f1b83811
jgkaplan <joshuagraykaplan@gmail.com>
2015-04-15 10:38:44 -0500

    Added SongTypes Enum

---
+++ Music_Thing/src/music_thing/SongTypes.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package music_thing;
+
+/**
+ *
+ * @author joshuakaplan
+ */
+public enum SongTypes {
+    MP3,WAV,MIDI,MP4,AIFF,FLAC,OGG
+}
