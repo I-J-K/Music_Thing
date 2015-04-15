b3807c92679782374d2124fe8319e74179ddf0cd
jgkaplan <joshuakaplan@10.150.102.220>
2015-04-15 10:49:27 -0500

    Added Quit to menubar (does not actually do anything yet)

--- Music_Thing/src/music_thing/FXMLDocumentController.java
+++ Music_Thing/src/music_thing/FXMLDocumentController.java
@@ -30,6 +30,9 @@ public class FXMLDocumentController implements Initializable {
     private MenuItem close;
     
     @FXML
+    private MenuItem quit;
+    
+    @FXML
     private void handleButtonAction(ActionEvent event) {
     }
     
