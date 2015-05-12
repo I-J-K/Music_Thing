0b0663923a0ed67a377c3f226828e0f254a8708f
jgkaplan <joshuagraykaplan@gmail.com>
2015-05-11 23:31:23 -0500

    Added support for OGG Vorbis file type and AU file type. Time slider now moves and time label updates.

--- Music_Thing/src/music_thing/ClipPlayer.java
+++ Music_Thing/src/music_thing/ClipPlayer.java
@@ -6,7 +6,10 @@
 package music_thing;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
@@ -22,9 +25,14 @@ import org.kc7bfi.jflac.sound.spi.FlacFormatConversionProvider;
  * 
  */
 public class ClipPlayer extends MusicPlayer{
-    private static Clip flacPlayer;
+    private static Clip clip;
     private static final FlacFormatConversionProvider flacStreamConverter = new FlacFormatConversionProvider();
     private static final FlacAudioFileReader flacReader = new FlacAudioFileReader();
+    
+    @Override
+    public int getSongLength(){
+        return (int)clip.getMicrosecondLength()/1000000;
+    }
     
     @Override
     public void reset(){
@@ -36,23 +44,46 @@ public class ClipPlayer extends MusicPlayer{
         stop();
         File file = new File("music/"+track.getPath());
         try{
-            flacPlayer = AudioSystem.getClip();
-            flacPlayer.open(flacStreamConverter.getAudioInputStream(AudioFormat.Encoding.PCM_SIGNED, flacReader.getAudioInputStream(file)));
+            clip = AudioSystem.getClip();
+            if(track.getType()==SongType.FLAC){
+                clip.open(flacStreamConverter.getAudioInputStream(AudioFormat.Encoding.PCM_SIGNED, flacReader.getAudioInputStream(file)));
+            }else if(track.getType()==SongType.OGG){
+                AudioInputStream in= AudioSystem.getAudioInputStream(file);
+                AudioInputStream din = null;
+                if (in != null){
+                    AudioFormat baseFormat = in.getFormat();
+                    AudioFormat  decodedFormat = new AudioFormat(
+                            AudioFormat.Encoding.PCM_SIGNED,
+                            baseFormat.getSampleRate(),
+                            16,
+                            baseFormat.getChannels(),
+                            baseFormat.getChannels() * 2,
+                            baseFormat.getSampleRate(),
+                            false);
+                     // Get AudioInputStream that will be decoded by underlying VorbisSPI
+                    din = AudioSystem.getAudioInputStream(decodedFormat, in);
+                    clip.open(din);
+                    in.close();
+                }
+            }
+            else{
+                clip.open(AudioSystem.getAudioInputStream(file));
+            }
         }catch(Exception e){System.out.println(e);}
     }
     
     @Override
     public double getSongTime(){
-        if(flacPlayer!=null){
-            return flacPlayer.getMicrosecondPosition()/1000000.0;
+        if(clip!=null){
+            return clip.getMicrosecondPosition()/1000000.0;
         }
         return 0.0;
     }
     
     @Override
     public void play(Track track, double volume){
-        if(flacPlayer==null || track!=getCurrentTrack())setSong(track);
-        flacPlayer.start();
+        if(clip==null || track!=getCurrentTrack())setSong(track);
+        clip.start();
         setPlaying(true);
         setCurrentTrack(track);
         setVolume(volume);
@@ -60,15 +91,15 @@ public class ClipPlayer extends MusicPlayer{
     
     @Override
     public void pause(){
-        flacPlayer.stop();
+        clip.stop();
         setPlaying(false);
     }
     
     @Override
     public void stop(){
-        if(flacPlayer!=null){
-            flacPlayer.stop();
-            flacPlayer.close();
+        if(clip!=null){
+            clip.stop();
+            clip.close();
         }
         setPlaying(false);
     }
@@ -76,7 +107,7 @@ public class ClipPlayer extends MusicPlayer{
     @Override
     public void setVolume(double volume){
         if(getCurrentTrack()!=null){
-            FloatControl c = (FloatControl) flacPlayer.getControl(FloatControl.Type.MASTER_GAIN);
+            FloatControl c = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
             c.setValue((float) volume*(c.getMaximum()-c.getMinimum())+c.getMinimum());
         }
     }
